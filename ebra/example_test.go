580af6e9768c691386b2050b853eca49c0b57c6f
marselester <marselester@ya.ru>
2019-03-24 19:19:26 +0700

    Add sparse vector implementation

---
+++ ebra/example_test.go
@@ -0,0 +1,41 @@
+package algebra_test
+
+import (
+	"fmt"
+
+	algebra "github.com/marselester/alg/ebra"
+)
+
+func ExampleSparseVector() {
+	a := [][]float64{
+		{0, .9, 0, 0, 0},
+		{0, 0, .36, .36, .18},
+		{0, 0, 0, .9, 0},
+		{.9, 0, 0, 0, 0},
+		{.47, 0, .47, 0, 0},
+	}
+	x := []float64{.05, .04, .36, .37, .19}
+	b := make([]float64, len(x))
+
+	var sum float64
+	for i := 0; i < len(a); i++ {
+		sum = 0
+		for j := 0; j < len(a[i]); j++ {
+			sum += a[i][j] * x[j]
+		}
+		b[i] = sum
+	}
+	fmt.Printf("%0.4f\n", b)
+
+	m := make([]*algebra.SparseVector, len(a))
+	for i := 0; i < len(a); i++ {
+		m[i] = algebra.NewSparseVector(a[i])
+	}
+	for i := 0; i < len(a); i++ {
+		b[i] = m[i].Dot(x)
+	}
+	fmt.Printf("%0.4f\n", b)
+	// Output:
+	// [0.0360 0.2970 0.3330 0.0450 0.1927]
+	// [0.0360 0.2970 0.3330 0.0450 0.1927]
+}
---
+++ ebra/sparse.go
@@ -0,0 +1,29 @@
+package algebra
+
+// SparseVector is used in matrix-vector multiplication when huge number of matrix entries are zero.
+// For example, when matrix size is 10 billion, but the average number of nonzero elements
+// per row is less than 10. For such an application, using symbol tables speeds up
+// matrix-vector multiplication by a factor of a billion.
+type SparseVector struct {
+	st map[int]float64
+}
+
+// NewSparseVector returns a new sparse vector created from the row.
+func NewSparseVector(row []float64) *SparseVector {
+	vec := SparseVector{st: make(map[int]float64)}
+	for i := range row {
+		if row[i] == 0 {
+			continue
+		}
+		vec.st[i] = row[i]
+	}
+	return &vec
+}
+
+// Dot computes a dot product of the sparse vector and the column.
+func (vec *SparseVector) Dot(column []float64) (sum float64) {
+	for k, v := range vec.st {
+		sum += column[k] * v
+	}
+	return sum
+}
