666b143fcb70ff46198d5738a5358a3f1f91fa3b
marselester <marselester@ya.ru>
2019-07-04 22:21:06 +0700

    Add example of tracking a path of the cycle

--- digraph/cycle.go
+++ digraph/cycle.go
@@ -15,10 +15,7 @@ A depth-first search solution is based on the fact that the recursive function c
 the "current" directed path under consideration.
 If we ever find a directed edge to a vertex that is on that stack, we have found a cycle,
 since the stack is evidence of a directed path.
-For example, in 0->5->4->3->5(check) call stack, the last "5" completes the cycle (3->5->4->3).
-
-If cycle exists, it's possible find the vertices from a vertex back to itself if you implement edgeTo
-which represents a last vertex on known path to a vertex.
+For example, in 0->5->4->3->5(check) call stack, the last "5" completes the cycle (3<-5<-4<-3).
 */
 func HasCycle(g *AdjacencyList) bool {
 	c := cycle{
@@ -59,6 +56,63 @@ func (c *cycle) dfs(source int) {
 			c.dfs(n.v)
 		case c.onStack[n.v]:
 			c.exists = true
+		}
+	}
+
+	c.onStack[source] = false
+}
+
+// Cycle returns a first cycle found in the directed graph. When cycle is detected,
+// it finds the vertices from last known source vertex back to itself.
+func Cycle(g *AdjacencyList) []int {
+	c := cyclepath{
+		g:       g,
+		onStack: make([]bool, g.VertexCount()),
+		marked:  make([]bool, g.VertexCount()),
+		edgeTo:  make([]int, g.VertexCount()),
+	}
+	for s := range g.a {
+		if !c.marked[s] {
+			c.dfs(s)
+		}
+	}
+	return c.cycle
+}
+
+type cyclepath struct {
+	g *AdjacencyList
+	// onStack is used to track the vertices for which recursive dfs call hasn't completed.
+	onStack []bool
+	// marked is an array of visited vertices.
+	marked []bool
+	// edgeTo holds a last vertex on known path to a vertex.
+	edgeTo []int
+	// cycle is the vertices that form a cycle.
+	cycle []int
+}
+
+func (c *cyclepath) dfs(source int) {
+	c.onStack[source] = true
+
+	c.marked[source] = true
+	for n := c.g.a[source]; n != nil; n = n.next {
+		switch {
+		// Cycle was already found.
+		case c.cycle != nil:
+			return
+		// Found the unvisited vertex, need to identify all vertices connected to it
+		// and remember how to get there (path to the unmarked vertex is from the source).
+		case !c.marked[n.v]:
+			c.edgeTo[n.v] = source
+			c.dfs(n.v)
+		// Cycle has just been detected, need to backtrack the path.
+		// For example, in 0->5->4->3->5(check) call stack, the last "5" completes the cycle (3<-5<-4<-3),
+		// where source=3 and n.v=5.
+		case c.onStack[n.v]:
+			for x := source; x != n.v; x = c.edgeTo[x] {
+				c.cycle = append(c.cycle, x)
+			}
+			c.cycle = append(c.cycle, n.v, source)
 		}
 	}
 
--- digraph/cycle_test.go
+++ digraph/cycle_test.go
@@ -1,6 +1,7 @@
 package digraph
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -32,4 +33,35 @@ func TestHasCycle(t *testing.T) {
 	if !HasCycle(g) {
 		t.Errorf("HasCycle() = false, want true")
 	}
+}
+
+func ExampleCycle() {
+	g := NewAdjacencyList(13)
+	g.Add(4, 2)
+	g.Add(2, 3)
+	g.Add(3, 2)
+	g.Add(6, 0)
+	g.Add(0, 1)
+	g.Add(2, 0)
+	g.Add(11, 12)
+	g.Add(12, 9)
+	g.Add(9, 10)
+	g.Add(9, 11)
+	g.Add(7, 9)
+	g.Add(10, 12)
+	g.Add(11, 4)
+	g.Add(4, 3)
+	g.Add(3, 5)
+	g.Add(6, 8)
+	g.Add(8, 6)
+	g.Add(5, 4)
+	g.Add(0, 5)
+	g.Add(6, 4)
+	g.Add(6, 9)
+	g.Add(7, 6)
+
+	// The expected cycle is 3<-5<-4<-3.
+	fmt.Println(Cycle(g))
+	// Output:
+	// [3 4 5 3]
 }
