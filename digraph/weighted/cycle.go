497b83b101a1cfb0399c015d491d09e505e3b841
marselester <marselester@ya.ru>
2020-03-21 16:33:17 -0400

    Add Cycle for weighted DAGs

--- digraph/weighted/cycle.go
+++ digraph/weighted/cycle.go
@@ -45,3 +45,60 @@ func (c *cycle) dfs(source int) {
 
 	c.onStack[source] = false
 }
+
+// Cycle returns a first cycle found in the directed graph. When cycle is detected,
+// it finds the vertices from last known source vertex back to itself.
+func Cycle(g *AdjacencyList) []int {
+	c := cyclepath{
+		g:       g,
+		onStack: make([]bool, g.VertexCount()),
+		marked:  make([]bool, g.VertexCount()),
+		edgeTo:  make([]int, g.VertexCount()),
+	}
+	for s := range g.a {
+		if !c.marked[s] {
+			c.dfs(s)
+		}
+	}
+	return c.cycle
+}
+
+type cyclepath struct {
+	g *AdjacencyList
+	// onStack is used to track the vertices for which recursive dfs call hasn't completed.
+	onStack []bool
+	// marked is an array of visited vertices.
+	marked []bool
+	// edgeTo holds a last vertex on known path to a vertex.
+	edgeTo []int
+	// cycle is the vertices that form a cycle.
+	cycle []int
+}
+
+func (c *cyclepath) dfs(source int) {
+	c.onStack[source] = true
+
+	c.marked[source] = true
+	for _, e := range c.g.Adjacent(source) {
+		switch {
+		// Cycle was already found.
+		case c.cycle != nil:
+			return
+		// Found the unvisited vertex, need to identify all vertices connected to it
+		// and remember how to get there (path to the unmarked vertex is from the source).
+		case !c.marked[e.W]:
+			c.edgeTo[e.W] = source
+			c.dfs(e.W)
+		// Cycle has just been detected, need to backtrack the path.
+		// For example, in 0->5->4->3->5(check) call stack, the last "5" completes the cycle (3<-5<-4<-3),
+		// where source=3 and n.v=5.
+		case c.onStack[e.W]:
+			for x := source; x != e.W; x = c.edgeTo[x] {
+				c.cycle = append(c.cycle, x)
+			}
+			c.cycle = append(c.cycle, e.W, source)
+		}
+	}
+
+	c.onStack[source] = false
+}
--- digraph/weighted/cycle_test.go
+++ digraph/weighted/cycle_test.go
@@ -1,6 +1,7 @@
 package weighted
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -32,4 +33,35 @@ func TestHasCycle(t *testing.T) {
 	if !HasCycle(g) {
 		t.Errorf("HasCycle() = false, want true")
 	}
+}
+
+func ExampleCycle() {
+	g := NewAdjacencyList(13)
+	g.Add(&Edge{4, 2, 0})
+	g.Add(&Edge{2, 3, 0})
+	g.Add(&Edge{3, 2, 0})
+	g.Add(&Edge{6, 0, 0})
+	g.Add(&Edge{0, 1, 0})
+	g.Add(&Edge{2, 0, 0})
+	g.Add(&Edge{11, 12, 0})
+	g.Add(&Edge{12, 9, 0})
+	g.Add(&Edge{9, 10, 0})
+	g.Add(&Edge{9, 11, 0})
+	g.Add(&Edge{7, 9, 0})
+	g.Add(&Edge{10, 12, 0})
+	g.Add(&Edge{11, 4, 0})
+	g.Add(&Edge{4, 3, 0})
+	g.Add(&Edge{3, 5, 0})
+	g.Add(&Edge{6, 8, 0})
+	g.Add(&Edge{8, 6, 0})
+	g.Add(&Edge{5, 4, 0})
+	g.Add(&Edge{0, 5, 0})
+	g.Add(&Edge{6, 4, 0})
+	g.Add(&Edge{6, 9, 0})
+	g.Add(&Edge{7, 6, 0})
+
+	// The expected cycle is 3<-5<-4<-3.
+	fmt.Println(Cycle(g))
+	// Output:
+	// [3 4 5 3]
 }
